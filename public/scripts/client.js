8cd602eb09898f6b502ecd4c2ce2d04aec096564
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-26 01:33:06 +0000

    changed milliseconds to day/hour/sec

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,59 +4,58 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-// const renderTweets = function(tweets) {
-//   // loops through tweets
-//   // calls createTweetElement for each tweet
-//   // takes return value and appends it to the tweets container
-// }
-
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+function convertMS( milliseconds ) {
+  var day, hour, minute, seconds;
+  seconds = Math.floor(milliseconds / 1000);
+  minute = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  hour = Math.floor(minute / 60);
+  minute = minute % 60;
+  day = Math.floor(hour / 24);
+  hour = hour % 24;
+  return {
+      day: day,
+      hour: hour,
+      minute: minute,
+      seconds: seconds
+  };
 }
 
 const createTweetElement = (tweetData) => {
   const { name, avatars, handle } = tweetData.user;
   const text = tweetData.content.text
-  const date = tweetData.created_at
+  const {day, hour, minute, seconds} = convertMS(tweetData.created_at)
+  const date = `${day}days ${hour}hours ${minute}mins ${seconds}s`
 
 let $tweet =
   `
-<article class="tweet>
+<article class="tweet">
 <section class="adding-form">
 <section class="flex-and-spacebetween">
   <div class="flex-and-spacebetween">
@@ -89,11 +88,18 @@ let $tweet =
 `
 return $tweet;
 }
+const renderTweets = (data) => {
+  for (let key of data) {
+    const $tweet = createTweetElement(key);
+    $('#tweets-container').append($tweet);
+  }
+}
 
 $(document).ready(() => {
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet);
+  // const $tweet = createTweetElement(tweetData);
+  // // Test / driver code (temporary)
+  // // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
+
